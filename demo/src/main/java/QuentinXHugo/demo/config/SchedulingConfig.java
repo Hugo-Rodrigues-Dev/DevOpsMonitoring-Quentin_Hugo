39acb919437e9642f2e724f3472c33a85136a5c9
Quentin Bouchot <113427532+QuentinB21@users.noreply.github.com>
2026-03-05 15:32:48 +0100

    Comment out VirtualThreadTaskExecutor configuration in SchedulingConfig

--- demo/src/main/java/QuentinXHugo/demo/config/SchedulingConfig.java
+++ demo/src/main/java/QuentinXHugo/demo/config/SchedulingConfig.java
@@ -2,14 +2,19 @@ package QuentinXHugo.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.VirtualThreadTaskExecutor;
+// import org.springframework.core.task.VirtualThreadTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 public class SchedulingConfig {
 
 	@Bean
-	public VirtualThreadTaskExecutor virtualThreadTaskExecutor() {
-		return new VirtualThreadTaskExecutor();
+	// public VirtualThreadTaskExecutor virtualThreadTaskExecutor() {
+	// 	return new VirtualThreadTaskExecutor();
+	public ThreadPoolTaskScheduler taskScheduler() {
+		ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+		scheduler.setPoolSize(4);
+		scheduler.setThreadNamePrefix("quest-scheduler-");
+		return scheduler;
 	}
 }
